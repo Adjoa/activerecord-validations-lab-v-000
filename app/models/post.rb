426ea73d1cc704e7a8cdc12f38d971331b168f62
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-13 13:56:01 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, presence: true, inclusion: { in: %w( "Won't Believe" "Secret" "Top [number]" "Guess"),
+     message: "%{value} should be clickbait-y" }
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
     message: "%{value} should be Fiction or Non-Fiction" }
-  # validates :title, inclusion: { in: %w( /\bWon't Believe\b/ "Secret" "Top [number]" "Guess"),
-  #     message: "%{value} should be clickbait-y" }
+   
 end
